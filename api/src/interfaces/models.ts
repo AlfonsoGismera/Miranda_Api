f718651d40469f3a2e6cf37e59ee3e0a81aff3fb
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-05-20 21:33:20 +0200

    update interfaces

---
+++ api/src/interfaces/models.ts
@@ -0,0 +1,46 @@
+export interface Room {
+  roomId: string;
+  roomName: string;
+  bedType: string;
+  roomFloor: string;
+  facilities: string[];
+  rate: string;
+  status: 'Available' | 'Booked' | 'Maintenance' | 'Out of Service';
+  image: string;
+}
+export interface Employee {
+  employeeId: string;
+  name: string;
+  image: string;
+  jobDesk: string;
+  schedule: string[];  
+  hireDate: string;
+  contact: string;
+  status: 'Active' | 'Inactive';
+}
+export interface Guest {
+  guest: string;
+  reservationId: string;
+  orderDate: string;
+  checkIn: string;
+  checkOut: string;
+  specialRequest: string;
+  roomType: string;
+  status: 'Checked In' | 'Pending' | 'Booked' | 'Cancelled' | 'Refunded';
+  email: string;
+  phone: string;
+  image: string;
+}
+export interface Guest {
+  guest: string;
+  reservationId: string;
+  orderDate: string;
+  checkIn: string;
+  checkOut: string;
+  specialRequest: string;
+  roomType: string;
+  status: 'Checked In' | 'Pending' | 'Booked' | 'Cancelled' | 'Refunded';
+  email: string;
+  phone: string;
+  image: string;
+}
