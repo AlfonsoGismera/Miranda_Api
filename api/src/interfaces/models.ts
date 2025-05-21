b9e04eb4a39dfa78ff65a6501b4556bc46871a00
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-05-21 09:41:59 +0200

    Update git ignore

--- api/src/interfaces/models.ts
+++ api/src/interfaces/models.ts
@@ -31,16 +31,3 @@ export interface Guest {
   phone: string;
   image: string;
 }
-export interface Guest {
-  guest: string;
-  reservationId: string;
-  orderDate: string;
-  checkIn: string;
-  checkOut: string;
-  specialRequest: string;
-  roomType: string;
-  status: 'Checked In' | 'Pending' | 'Booked' | 'Cancelled' | 'Refunded';
-  email: string;
-  phone: string;
-  image: string;
-}
